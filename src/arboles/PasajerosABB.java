ec7bc649c151dfb5293e8cb0d2d5a4f0ae7f4557
ecustiel <ecustiel@gmail.com>
2022-04-04 17:07:12 -0300

    Se agregan funciones a PasajerosABB, falta correccion del profesor

--- src/arboles/PasajerosABB.java
+++ src/arboles/PasajerosABB.java
@@ -1,13 +1,19 @@
 package arboles;
 
-import org.jetbrains.annotations.NotNull;
+//import org.jetbrains.annotations.NotNull;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PasajerosABB {
 
-    private NodoPasajero pasajero;
+    //Data
+    private NodoPasajero pasajeros;
+
+    //Getter
+    public NodoPasajero getPasajeros() {
+        return pasajeros;
+    }
 
 
     //Funcion para Comprobar validez de Cedula (Formato 4.549.685) sin verificador
@@ -17,6 +23,55 @@ public class PasajerosABB {
         Matcher matcher = patron.matcher(cedula);
 
         return matcher.matches();
+    }
+
+
+    //Insertar Pasajero
+    //public String insertarPasajero(String cedula, String nombre, String telefrono, Categoria categoria) {
+
+    //}
+
+
+    //Comprobar Si Existe y devolverlo, hacemos uso de la funcion pasajeroExiste
+    public String buscarPasajero(String cedula){
+
+        if(comprobarCi(cedula)){
+            return pasajeroExiste(cedula, this.pasajeros);
+        }else{
+            return "No Existe";
+        }
+
+    }
+
+
+    //Contador de Nodos
+    public int cantidadNodos(NodoPasajero pas){
+        int cont = 0;
+        if(pas != null){
+            cont += cantidadNodos(pas.getIzq());
+            cont++;
+            cont += cantidadNodos(pas.getDer());
+        }
+
+        return cont;
+    }
+
+
+    //Funcion para comprobar si el pasajero existe, devuelve los datos
+    private String pasajeroExiste(String cedula, NodoPasajero pas){
+        String vacio;
+        if(pas == null){
+            return vacio = "";
+        }else{
+            if(pas.getPasajero().getCedula().compareTo(cedula) == 0)
+                return pas.getPasajero().getCedula() + ";" + pas.getPasajero().getNombre() + ";"
+                        + pas.getPasajero().getTelefono() + ";" ; //Falta Categoria
+            else if (pas.getPasajero().getCedula().compareTo(cedula) > 0) {
+                return pasajeroExiste(cedula, pas.getIzq());
+            } else
+                return pasajeroExiste(cedula, pas.getDer());
+        }
+
     }
 
 }
